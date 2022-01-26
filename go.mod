d3189c61f3e52835637ff0953bed0bdc5e8eb911
s1at3 <s1at3@protonmail.com>
2022-01-26 13:34:54 +0900

    Added additional comments and moved some code out of the main function into separate functions.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/s1at3/Go_Concurrent_PortScanner
+
+go 1.17
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
+/*
+
+This scanner currently only operates on a single host, not a range of hosts, however
+I will attempt to add this functionality at a later point. The extent of concurrent
+connections is determined by the user, but defaults to 10. You may need to raise the
+limit on the number of open files / connections if using MacOS or Linux with the
+ulimit -n [max#] command if you wish to push the number of workers past a few hundred.
+
+It also attemps to unintelligently grab a banner by taking the first 1024 bytes of a
+connection. I may add attempts to try some protocol-specific requests against open
+ports to enumerate additional details.
+
+*/
+
 package main
 
 import (
 	"bytes"
-	//"encoding/json"
-	//	"encoding/binary"
-	//"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -21,6 +32,7 @@ import (
 	"time"
 )
 
+//Port is just a structure for basic port details
 type Port struct {
 	PortNum uint16
 	Open    bool
@@ -40,7 +52,7 @@ func main() {
 
 	flag.Parse()
 
-	limitCheck(*workers)
+	LimitCheck(*workers)
 
 	if *hostPtr == "" || *startPort == 0 || *endPort == 0 {
 		log.Fatal("Host, starting port, and end port cannot be blank.")
@@ -48,38 +60,96 @@ func main() {
 		log.Fatal("Invalid port range.")
 	}
 
-	f, err := os.Create(*outputFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
 	sem := make(chan struct{}, *workers)
 	resultsChan := make(chan Port)
-	openPorts := []Port{}
-
 	wg := &sync.WaitGroup{}
 
-	ports := portRange(*startPort, *endPort)
+	ports := portRange(*startPort, *endPort, *randomize)
 
-	if *randomize == true {
-		rand.Shuffle(len(ports), func(i, j int) {
-			ports[i], ports[j] = ports[j], ports[i]
+	openPorts := CreateWorkers(ports, *hostPtr, *timeOut, wg, resultsChan, sem)
+
+	close(sem)
+	close(resultsChan)
+
+	ParseResults(openPorts, *outputFile, *openOnly)
+
+}
+
+func portRange(start, stop uint, randomize bool) []uint16 {
+
+	portList := []uint16{}
+
+	for i := start; i <= stop; i++ {
+		portList = append(portList, uint16(i))
+	}
+
+	if randomize == true {
+		rand.Shuffle(len(portList), func(i, j int) {
+			portList[i], portList[j] = portList[j], portList[i]
 		})
 	}
+
+	return portList
+
+}
+
+func portOpen(host string, port uint16, timeout time.Duration, wg *sync.WaitGroup, resultsChan chan<- Port, sem chan struct{}) {
+	defer wg.Done()
+
+	var buf bytes.Buffer
+	banner := ""
+
+	tgt := host + ":" + strconv.FormatUint(uint64(port), 10)
+	conn, err := net.DialTimeout("tcp", tgt, timeout)
+
+	if err != nil {
+		if err, _ := err.(net.Error); err.Timeout() {
+			banner = "Timed Out"
+		}
+		resultsChan <- Port{PortNum: port, Open: false, Banner: banner}
+	} else {
+		defer conn.Close()
+		fmt.Printf("[i] Found open port: %d\n", port)
+		conn.SetDeadline(time.Now().Add(timeout))
+		connData := io.LimitReader(conn, 1024)
+		io.Copy(&buf, connData)
+		banner = strings.TrimSpace(buf.String())
+		conn.Close()
+		resultsChan <- Port{PortNum: port, Open: true, Banner: banner}
+	}
+	<-sem
+}
+
+//CreateWorkers does what it says, creating up to the maximum amount of workers
+//The sem channel blocks execution once it fills up with the max workers, waiting
+//to empty a bit before continuing on.
+func CreateWorkers(ports []uint16, host string, timeOut time.Duration, wg *sync.WaitGroup, resultsChan chan Port, sem chan struct{}) []Port {
+	openPorts := []Port{}
 
 	for i := 0; i < len(ports); i++ {
 		sem <- struct{}{}
 		wg.Add(1)
-		go portOpen(*hostPtr, ports[i], *timeOut, wg, resultsChan, sem)
+		go portOpen(host, ports[i], timeOut, wg, resultsChan, sem)
 		go func() {
 			result := <-resultsChan
 			openPorts = append(openPorts, result)
 		}()
-
 	}
 
 	wg.Wait()
+	return openPorts
+}
+
+//ParseResults takes the slice of open ports, iterating through and formatting them
+//line by line before writing to a target file. At some point will add the ability
+//to export to different file formats (JSON, XML, plaintext, etc)
+func ParseResults(openPorts []Port, filename string, openOnly bool) {
+
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
 	sort.Slice(openPorts, func(i, j int) bool {
 		return openPorts[i].PortNum < openPorts[j].PortNum
@@ -90,7 +160,7 @@ func main() {
 		if result.Open == true {
 			status = "open"
 		} else {
-			if *openOnly == true {
+			if openOnly == true {
 				continue
 			}
 			status = "closed"
@@ -105,50 +175,7 @@ func main() {
 		}
 		f.WriteString(resultLine)
 	}
-
 }
-
-func portRange(start, stop uint) []uint16 {
-
-	portList := []uint16{}
-
-	for i := start; i <= stop; i++ {
-		portList = append(portList, uint16(i))
-	}
-
-	return portList
-
-}
-
-func portOpen(host string, port uint16, timeout time.Duration, wg *sync.WaitGroup, resultsChan chan<- Port, sem chan struct{}) {
-	defer wg.Done()
-
-	result := Port{}
-
-	t := host + ":" + strconv.FormatUint(uint64(port), 10)
-	conn, err := net.DialTimeout("tcp", t, timeout)
-	banner := ""
-	if err != nil {
-		if err, _ := err.(net.Error); err.Timeout() {
-			banner = "Timed Out"
-		}
-		result = Port{PortNum: port, Open: false, Banner: banner}
-	} else {
-		defer conn.Close()
-		fmt.Printf("[i] Found open port: %d\n", port)
-		conn.SetDeadline(time.Now().Add(timeout))
-		var buf bytes.Buffer
-		thing := io.LimitReader(conn, 1024)
-		io.Copy(&buf, thing)
-		banner = strings.TrimSpace(buf.String())
-		conn.Close()
-		result = Port{PortNum: port, Open: true, Banner: banner}
-	}
-	resultsChan <- result
-	<-sem
-}
-
-func CreateWorkers() {}
 
 // LimitCheck compares your worker count to the maximum number of open "files" or
 // connections if running Linux or MacOS. If greater than 90% capaicty, exits.
